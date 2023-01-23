9d0fcffb0ac275d20499e447950fc11ec1b40554
sachinaravind92 <122426999+sachinaravind92@users.noreply.github.com>
2023-01-23 11:50:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    source = "hashicorp/aws"
+    version = "~> 4.16"
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "first_instance" {
+  ami = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+
+  tags {
+    name = "saravind"
+  }
+}
+
